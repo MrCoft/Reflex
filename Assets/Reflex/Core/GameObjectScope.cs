96fc1d50960fdafc73ebe1e6b55423973034b381
MrCoft <devcoft@gmail.com>
2025-10-28 13:21:03 +0100

    add GameObjectScope

---
+++ Assets/Reflex/Core/GameObjectScope.cs
@@ -0,0 +1,27 @@
+﻿using Reflex.Logging;
+using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Reflex.Core
+{
+    public sealed class GameObjectScope : MonoBehaviour
+    {
+        public static Action<GameObject, ContainerBuilder> OnGameObjectContainerBuilding;
+
+        public Container Container;
+        
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            using var pooledObject = ListPool<IInstaller>.Get(out var installers);
+            GetComponentsInChildren<IInstaller>(installers);
+
+            for (var i = 0; i < installers.Count; i++)
+            {
+                installers[i].InstallBindings(containerBuilder);
+            }
+
+            ReflexLogger.Log($"GameObjectScope ({gameObject.name}) Bindings Installed", LogLevel.Info, gameObject);
+        }
+    }
+}
--- Assets/Reflex/Injectors/GameObjectInjector.cs
+++ Assets/Reflex/Injectors/GameObjectInjector.cs
@@ -47,17 +47,84 @@ namespace Reflex.Injectors
             }
         }
 
-        public static void InjectRecursiveMany(List<GameObject> gameObject, Container container)
+        public static void InjectRecursiveMany(List<GameObject> gameObjects, Container container)
         {
             using var pooledObject = ListPool<MonoBehaviour>.Get(out var monoBehaviours);
+            using var pooledGameObjectScope = ListPool<GameObjectScope>.Get(out var gameObjectScopes);
+            using var pooledGameObjectScopeTree = HashSetPool<Transform>.Get(out var gameObjectScopeTree);
 
-            for (var i = 0; i < gameObject.Count; i++)
+            for (var i = 0; i < gameObjects.Count; i++)
             {
-                gameObject[i].GetComponentsInChildren<MonoBehaviour>(true, monoBehaviours);
-
-                for (var j = 0; j < monoBehaviours.Count; j++)
+                var gameObject = gameObjects[i];
+                
+                gameObject.GetComponentsInChildren<GameObjectScope>(true, gameObjectScopes);
+                
+                gameObjectScopeTree.Clear();
+                for (var j = 0; j < gameObjectScopes.Count; j++)
                 {
-                    var monoBehaviour = monoBehaviours[j];
+                    var treeNode = gameObjectScopes[j].transform;
+
+                    while (treeNode != null)
+                    {
+                        gameObjectScopeTree.Add(treeNode);
+                        treeNode = treeNode.parent;
+                    }
+                }
+                
+                InjectRecursiveWithPossibleGameObjectScopes(
+                    gameObject,
+                    container,
+                    monoBehaviours,
+                    gameObjectScopeTree);
+            }
+        }
+
+        public static void InjectRecursiveWithPossibleGameObjectScopes(
+            GameObject gameObject,
+            Container container,
+            List<MonoBehaviour> monoBehaviours,
+            HashSet<Transform> gameObjectScopeTree)
+        {
+            if (gameObjectScopeTree.Contains(gameObject.transform))
+            {
+                var gameObjectScope = gameObject.GetComponent<GameObjectScope>();
+                
+                if (gameObjectScope != null)
+                {
+                    InjectGameObjectScope(gameObjectScope, container);
+                    return;
+                }
+                
+                // todo - include inactive?
+                gameObject.GetComponents<MonoBehaviour>(monoBehaviours);
+
+                for (var i = 0; i < monoBehaviours.Count; i++)
+                {
+                    var monoBehaviour = monoBehaviours[i];
+
+                    if (monoBehaviour != null)
+                    {
+                        AttributeInjector.Inject(monoBehaviour, container);
+                    }
+                }
+                
+                var childrenCount = gameObject.transform.childCount;
+                for (var i = 0; i < childrenCount; i++)
+                {
+                    InjectRecursiveWithPossibleGameObjectScopes(
+                        gameObject.transform.GetChild(i).gameObject,
+                        container,
+                        monoBehaviours,
+                        gameObjectScopeTree);
+                }
+            }
+            else
+            {
+                gameObject.GetComponentsInChildren<MonoBehaviour>(monoBehaviours);
+
+                for (var i = 0; i < monoBehaviours.Count; i++)
+                {
+                    var monoBehaviour = monoBehaviours[i];
 
                     if (monoBehaviour != null)
                     {
@@ -65,6 +132,26 @@ namespace Reflex.Injectors
                     }
                 }
             }
+        }
+        
+        private static void InjectGameObjectScope(GameObjectScope gameObjectScope, Container container)
+        {
+            var childContainer = container.Scope(builder =>
+            {
+                builder.SetName($"{gameObjectScope.name} ({gameObjectScope.GetHashCode()})");
+                gameObjectScope.InstallBindings(builder);
+                GameObjectScope.OnGameObjectContainerBuilding?.Invoke(gameObjectScope.gameObject, builder);
+            });
+            gameObjectScope.Container = childContainer;
+            
+            using var pooledObject = ListPool<GameObject>.Get(out var children);
+            var childrenCount = gameObjectScope.transform.childCount;
+            for (var i = 0; i < childrenCount; i++)
+            {
+                children.Add(gameObjectScope.transform.GetChild(i).gameObject);
+            }
+            
+            InjectRecursiveMany(children, childContainer);
         }
     }
 }
